895b410904a2f87fd295296b801914aca2f4acfb
Will Thames <wthames@skedulo.com>
2019-05-01 14:39:38 +1000

    Fix root context properly for static files

--- server.py
+++ server.py
@@ -6,12 +6,11 @@ from colour import colour
 import os
 from threading import Lock
 
-from flask import Flask, render_template, make_response, request, session
+from flask import Flask, render_template, make_response, request, session, Blueprint
 from flask_socketio import SocketIO, emit, join_room, leave_room
 from flask_socketio import close_room, rooms, disconnect
 
 
-ROOT_CONTEXT = os.environ.get('ROOT_CONTEXT', '/')
 DEBUG = bool(os.environ.get('DEBUG', False))
 
 
@@ -22,7 +21,12 @@ thread = None
 thread_lock = Lock()
 
 
-@app.route(ROOT_CONTEXT)
+bp = Blueprint('docker-debug', __name__,
+               template_folder='templates',
+               static_folder='static')
+
+
+@bp.route('/')
 def docker_debug():
     with open(os.environ.get("WWW_DATA", "helloworld.txt")) as f:
         data = f.read()
@@ -34,10 +38,13 @@ def docker_debug():
     return response
 
 
-@app.route(f'{ROOT_CONTEXT}/ws', methods=['GET', 'POST'])
+@bp.route('/ws', methods=['GET', 'POST'])
 def websocket_test():
     return make_response(render_template('ws.j2',
-                                         namespace=f'{ROOT_CONTEXT}/ws'))
+                                         namespace='/ws'))
+
+
+app.register_blueprint(bp, url_prefix=os.environ.get('ROOT_CONTEXT', '/'))
 
 
 def background_thread():
@@ -48,17 +55,17 @@ def background_thread():
         count += 1
         socketio.emit('my_response',
                       {'data': 'Server generated event', 'count': count},
-                      namespace=f'{ROOT_CONTEXT}/ws')
+                      namespace='/ws')
 
 
-@socketio.on('my_event', namespace=f'{ROOT_CONTEXT}/ws')
+@socketio.on('my_event', namespace='/ws')
 def test_message(message):
     session['receive_count'] = session.get('receive_count', 0) + 1
     emit('my_response',
          {'data': message['data'], 'count': session['receive_count']})
 
 
-@socketio.on('my_broadcast_event', namespace=f'{ROOT_CONTEXT}/ws')
+@socketio.on('my_broadcast_event', namespace='/ws')
 def test_broadcast_message(message):
     session['receive_count'] = session.get('receive_count', 0) + 1
     emit('my_response',
@@ -66,7 +73,7 @@ def test_broadcast_message(message):
          broadcast=True)
 
 
-@socketio.on('join', namespace=f'{ROOT_CONTEXT}/ws')
+@socketio.on('join', namespace='/ws')
 def join(message):
     join_room(message['room'])
     session['receive_count'] = session.get('receive_count', 0) + 1
@@ -75,7 +82,7 @@ def join(message):
           'count': session['receive_count']})
 
 
-@socketio.on('leave', namespace=f'{ROOT_CONTEXT}/ws')
+@socketio.on('leave', namespace='/ws')
 def leave(message):
     leave_room(message['room'])
     session['receive_count'] = session.get('receive_count', 0) + 1
@@ -84,7 +91,7 @@ def leave(message):
           'count': session['receive_count']})
 
 
-@socketio.on('close_room', namespace=f'{ROOT_CONTEXT}/ws')
+@socketio.on('close_room', namespace='/ws')
 def close(message):
     session['receive_count'] = session.get('receive_count', 0) + 1
     emit('my_response', {'data': 'Room ' + message['room'] + ' is closing.',
@@ -93,7 +100,7 @@ def close(message):
     close_room(message['room'])
 
 
-@socketio.on('my_room_event', namespace=f'{ROOT_CONTEXT}/ws')
+@socketio.on('my_room_event', namespace='/ws')
 def send_room_message(message):
     session['receive_count'] = session.get('receive_count', 0) + 1
     emit('my_response',
@@ -101,7 +108,7 @@ def send_room_message(message):
          room=message['room'])
 
 
-@socketio.on('disconnect_request', namespace=f'{ROOT_CONTEXT}/ws')
+@socketio.on('disconnect_request', namespace='/ws')
 def disconnect_request():
     session['receive_count'] = session.get('receive_count', 0) + 1
     emit('my_response',
@@ -109,12 +116,12 @@ def disconnect_request():
     disconnect()
 
 
-@socketio.on('my_ping', namespace=f'{ROOT_CONTEXT}/ws')
+@socketio.on('my_ping', namespace='/ws')
 def ping_pong():
     emit('my_pong')
 
 
-@socketio.on('connect', namespace=f'{ROOT_CONTEXT}/ws')
+@socketio.on('connect', namespace='/ws')
 def test_connect():
     global thread
     with thread_lock:
@@ -123,7 +130,7 @@ def test_connect():
     emit('my_response', {'data': 'Connected', 'count': 0})
 
 
-@socketio.on('disconnect', namespace=f'{ROOT_CONTEXT}/ws')
+@socketio.on('disconnect', namespace='/ws')
 def test_disconnect():
     print('Client disconnected', request.sid)
 
